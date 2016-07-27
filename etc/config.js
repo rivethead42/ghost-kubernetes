7d3e2d14210eaa5076aa56880baed62fc45effc5
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-07-27 11:52:21 +0200

    Fix syntax error in config

--- etc/config.js
+++ etc/config.js
@@ -61,7 +61,7 @@ config = {
             host     : process.env.MYSQL_HOST,
             user     : process.env.MYSQL_USER,
             password : process.env.MYSQL_PASS,
-            database : process.env.MYSQL_DB',
+            database : process.env.MYSQL_DB,
             charset  : 'utf8'
           }
         },
